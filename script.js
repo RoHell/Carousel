f21e8abac213bce07aa7279287baf7b481fe6ca5
Karol Mąkowski <Karol Mkowski>
2016-07-05 21:10:09 +0200

    Andy example code

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-$(function() {
-	var carouselList = $("#carousel ul");
-});
-
-
 $(function changeSlide() {
+	var carouselList = $("#carousel ul");
 	setInterval(changeSlide, 3000);
-	carouselList.animate({marginLeft:-500}, 500, moveFirstSlide);
+	carouselList.animate({marginLeft:-600}, 500, moveFirstSlide);
 });
 
 
@@ -14,4 +10,40 @@ $(function moveFirstSlide() {
 	var lastItem = carouselList.find("li:last");
 	lastItem.after(firstItem);
 	carouselList.css({marginLeft:0});
+});
+
+
+var carouselList = $(".carousel ul");
+// // setTimeout(rotate, 2000);
+// setInterval(rotate, 3000);
+
+function rotate() {
+	carouselList.animate({"margin-left": -600}, 1000, moveFirstSlide);	
+};
+
+function moveFirstSlide() {
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+	lastItem.after(firstItem);
+	carouselList.css({marginLeft:0});
+};
+
+function rotate1() {
+	carouselList.animate({"margin-left": 0}, 1000, moveFirstSlide1);	
+};
+
+function moveFirstSlide1() {
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+	lastItem.insertBefore(firstItem);
+	carouselList.css({marginLeft:-600});
+};
+
+
+$("#right").click(function() {
+	rotate();
+});
+
+$("#left").click(function() {
+	rotate1();
 });
