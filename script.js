f8f2dadadc9f6fbc403d8e27ab8650607d6a66bb
RoHell <karol-roch@wp.pl>
2016-07-06 15:27:25 +0200

    slides working

--- script.js
+++ script.js
@@ -1,49 +1,35 @@
-$(function changeSlide() {
-	var carouselList = $("#carousel ul");
-	setInterval(changeSlide, 3000);
-	carouselList.animate({marginLeft:-600}, 500, moveFirstSlide);
-});
+	var carouselList = $(".carousel ul");
 
+	$(function () {
+		setInterval(changeSlideRight, 3000);
+	});
 
-$(function moveFirstSlide() {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-});
+	function changeSlideRight() {
+		carouselList.animate({marginLeft: "-600px"}, 500, firstSlideRight);
+	};
 
+	function changeSlideLeft() {
+		carouselList.animate({marginLeft: "0"}, 500, firstSlideLeft);
+	};
 
-var carouselList = $(".carousel ul");
-// // setTimeout(rotate, 2000);
-// setInterval(rotate, 3000);
+	function firstSlideRight() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft: "0"});
+	};
 
-function rotate() {
-	carouselList.animate({"margin-left": -600}, 1000, moveFirstSlide);	
-};
+	function firstSlideLeft() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft: "-600px"});
+	};
 
-function moveFirstSlide() {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-};
+	$("#right").click(function() {
+		changeSlideRight();
+	});
 
-function rotate1() {
-	carouselList.animate({"margin-left": 0}, 1000, moveFirstSlide1);	
-};
-
-function moveFirstSlide1() {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	lastItem.insertBefore(firstItem);
-	carouselList.css({marginLeft:-600});
-};
-
-
-$("#right").click(function() {
-	rotate();
-});
-
-$("#left").click(function() {
-	rotate1();
-});
+	$("#left").click(function() {
+		changeSlideLeft();
+	});
