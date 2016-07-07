015549878902f42207b48fada6e93c791f64619a
RoHell <karol-roch@wp.pl>
2016-07-07 15:58:02 +0200

    added circles

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-	var carouselList = $(".carousel ul");
+	var $carouselList = $(".gallery");
+	var $circleList = $(".circle");
+	var imgNumber = 
 
 	$(function () {
 		setInterval(changeSlideRight, 3000);
 	});
 
 	function firstSlideRight() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft: "0"});
+		var $firstItem = $carouselList.find("li:first");
+		var $lastItem = $carouselList.find("li:last");
+		$lastItem.after($firstItem);
+		$carouselList.css({marginLeft: "0"});
 	};
 
 	function changeSlideRight() {
-		carouselList.animate({marginLeft: "-600px"}, 500, firstSlideRight);
+		$carouselList.animate({marginLeft: "-600px"}, 500, firstSlideRight);
 	};
 
 	function firstSlideLeft() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-		lastItem.before(firstItem);
-		carouselList.css({marginLeft: "-600px"});
+		var $firstItem = $carouselList.find("li:first");
+		var $lastItem = $carouselList.find("li:last");
+		$lastItem.before($firstItem);
+		$carouselList.css({marginLeft: "-600px"});
 	};
 
 	function changeSlideLeft() {
-		carouselList.animate({marginLeft: "0"}, 500, firstSlideLeft);
+		$carouselList.animate({marginLeft: "0"}, 500, firstSlideLeft);
 	};
 
 	$("#right").click(function() {
@@ -33,3 +35,12 @@
 	$("#left").click(function() {
 		changeSlideLeft();
 	});
+
+	$(".circle li").click(function() {
+		$(this).toggleClass("disc");
+		$(.circle li).css({listStyleType: "circle"})
+	});
+
+
+
+
