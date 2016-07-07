7cfc98830f096c772194153b63f445df9387e66b
Karol Mąkowski <Karol Mkowski>
2016-07-07 16:20:32 +0200

    hhh

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 	var $carouselList = $(".gallery");
 	var $circleList = $(".circle");
-	var imgNumber = 
+	// var imgNumber = 
 
-	$(function () {
-		setInterval(changeSlideRight, 3000);
-	});
+	// $(function () {
+	// 	setInterval(changeSlideRight, 3000);
+	// });
 
 	function firstSlideRight() {
 		var $firstItem = $carouselList.find("li:first");
@@ -25,7 +25,8 @@
 	};
 
 	function changeSlideLeft() {
-		$carouselList.animate({marginLeft: "0"}, 500, firstSlideLeft);
+		firstSlideLeft();
+		$carouselList.animate({marginLeft: "0"}, 500);
 	};
 
 	$("#right").click(function() {
@@ -38,7 +39,7 @@
 
 	$(".circle li").click(function() {
 		$(this).toggleClass("disc");
-		$(.circle li).css({listStyleType: "circle"})
+		$('.circle li').css({listStyleType: "circle"})
 	});
 
 
